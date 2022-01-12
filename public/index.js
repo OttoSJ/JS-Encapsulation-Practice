1585ee948a5250c65608a9eef050f43de7fb440e
Otto Jones <ottosjonesjr@gmail.com>
2022-01-12 12:22:35 -0500

    Encapsulated fucntions in both the ingredientList.js and cart.js into an object andrefractored code to refelect the changes. Tested and debuged code to make sure all functions still work. Resolved all errors.

--- public/index.js
+++ public/index.js
@@ -9,31 +9,31 @@ async function main() {
     items = fetchedCartItems
 
     // Select the first sandwich on page load
-    selectSandwich(items[0])
+    renderingContent.selectSandwich(items[0])
 
     // Display the ingredients we fetched
-    renderIngredientList();
+    ingredientList.render();
 
     // Display the sandwiches we fetched
-    renderCart();
+    renderingContent.renderCart();
 
     // Attach event listener to the 'Add Sandwich' button
     let addButton = document.querySelector('.add-button');
     addButton.addEventListener('click', () => {
-        addSandwich()
+        renderingContent.addSandwich()
     })
 
     // Attach event listener to the 'Name' input
     let nameInput = document.querySelector('.name-input');
     nameInput.addEventListener('input', (e) => {
-        changeSelectedSandwichName(e.target.value)
+        renderingContent.changeSelectedSandwichName(e.target.value)
     })
 
     // Attach event listeners to the radio inputs for selecting a type of bread
     let breadRadios = document.querySelectorAll('.bread-radio')
     breadRadios.forEach(radioInput => {
         radioInput.addEventListener('click', () => {
-            changeSelectedSandwichBread(radioInput.value)
+            renderingContent.changeSelectedSandwichBread(radioInput.value)
         })
     })
 }
